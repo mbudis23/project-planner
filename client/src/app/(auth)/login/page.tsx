d644de3167b3d5f95bfb6b752ff4800884033b5a
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2024-09-25 14:07:18 +0700

    Add Login Page

---
+++ client/src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginCard from "@/components/scoped/login/LoginCard";
+
+export default function LoginPage(){
+    return(
+        <main className="min-h-screen min-w-full bg-white text-center font-montserrat flex justify-center items-center text-black">
+            <LoginCard/>
+        </main>
+    )
+}
